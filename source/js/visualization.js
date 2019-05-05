4d7c210d63be7736b0febefa3855b5c454325c57
MaxSamartsev <45741458+MaxSamartsev@users.noreply.github.com>
2019-05-05 18:26:39 +0300

    Переписал слайдер на JS + СSSCombo на CSS

--- source/js/visualization.js
+++ source/js/visualization.js
@@ -1,23 +1,21 @@
+var visualBlock = document.querySelector('.visualization__wrapper');
+
+visualBlock.classList.add('visualization__wrapper--before');
+
 var labelBefore = document.querySelector('.visualization__label--before');
 var labelAfter = document.querySelector('.visualization__label--after');
-var labelButton = document.querySelector('.visualization__label');
-var inputBefore = document.querySelector('.visualization__input--before');
 
-if (inputBefore.checked) {
-  labelBefore.classList.add('visualization__label--active');}
-  else (labelAfter.classList.add('visualization__label--active'));
-
-labelBefore.addEventListener('click', function () {
-  if (labelAfter.classList.contains('visualization__label--active')) {
-    labelAfter.classList.remove('visualization__label--active');
-    labelBefore.classList.add('visualization__label--active');
+labelAfter.addEventListener('click', function () {
+  if (visualBlock.classList.contains('visualization__wrapper--before')) {
+    visualBlock.classList.remove('visualization__wrapper--before');
+    visualBlock.classList.add('visualization__wrapper--after');
   }
 });
 
-labelAfter.addEventListener('click', function () {
-  if (labelBefore.classList.contains('visualization__label--active')) {
-    labelBefore.classList.remove('visualization__label--active');
-    labelAfter.classList.add('visualization__label--active');
+labelBefore.addEventListener('click', function () {
+  if (visualBlock.classList.contains('visualization__wrapper--after')) {
+    visualBlock.classList.remove('visualization__wrapper--after');
+    visualBlock.classList.add('visualization__wrapper--before');
   }
 });
 
