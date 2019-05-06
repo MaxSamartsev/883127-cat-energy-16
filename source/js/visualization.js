04c06bf1717a46c22d7fc2d8cc55bf567a3de9b3
MaxSamartsev <45741458+MaxSamartsev@users.noreply.github.com>
2019-05-07 02:15:46 +0300

    Выполнил JS код карты

--- source/js/visualization.js
+++ source/js/visualization.js
@@ -1,20 +1,150 @@
-var visualBlock = document.querySelector('.visualization__wrapper');
 
-visualBlock.classList.add('visualization__wrapper--before');
+var tabletWidth = 768;
+var LAPTOP_WIDTH = 1024;
+var visual = document.querySelector(".visualization__wrapper");
+var btnBefore = visual.querySelector(".visualization__label--before");
+var btnlAfter = visual.querySelector(".visualization__label--after");
+var scale = visual.querySelector(".visualization__scale");
+var grip = visual.querySelector(".visualization__grip");
+var imageBefore = visual.querySelector(".visualization__image--before");
+var imageAfter = visual.querySelector(".visualization__image--after");
+var scaleWidth;
+var gripWidth;
+var sliderWidth;
 
-var labelBefore = document.querySelector('.visualization__label--before');
-var labelAfter = document.querySelector('.visualization__label--after');
+visual.classList.add('visualization__wrapper--before');
 
-labelAfter.addEventListener('click', function () {
-  if (visualBlock.classList.contains('visualization__wrapper--before')) {
-    visualBlock.classList.remove('visualization__wrapper--before');
-    visualBlock.classList.add('visualization__wrapper--after');
+btnlAfter.addEventListener('click', function () {
+  if (visual.classList.contains('visualization__wrapper--before')) {
+    visual.classList.remove('visualization__wrapper--before');
+    visual.classList.add('visualization__wrapper--after');
   }
 });
 
-labelBefore.addEventListener('click', function () {
-  if (visualBlock.classList.contains('visualization__wrapper--after')) {
-    visualBlock.classList.remove('visualization__wrapper--after');
-    visualBlock.classList.add('visualization__wrapper--before');
+btnBefore.addEventListener('click', function () {
+  if (visual.classList.contains('visualization__wrapper--after')) {
+    visual.classList.remove('visualization__wrapper--after');
+    visual.classList.add('visualization__wrapper--before');
   }
 });
+
+//////////
+
+
+(function () {
+
+  var getElemWidth = function (elem) {
+    return parseInt(getComputedStyle(elem).width, 10);
+  };
+
+  btnBefore.onclick = function (evt) {
+    evt.preventDefault();
+    before.style.width = "100%";
+    after.style.width = "0";
+    grip.style.marginLeft = "0";
+    grip.style.transition = "margin-left 2.5s ease-in-out";
+    before.style.transition = "width 2s ease-in-out";
+
+    if (viewport>=LAPTOP_WIDTH) {
+      grip.style.transition = "margin-left 3s ease-in-out";
+      before.style.transition = "width 3s ease-in-out";
+    }
+    else {
+      grip.style.transition = "margin-left 3s ease-in-out";
+      before.style.transition = "width 1.5s ease-in-out";
+    }
+  };
+
+  btnAfter.onclick = function (evt) {
+    evt.preventDefault();
+    before.style.width = "0";
+    after.style.width = "100%";
+    grip.style.marginLeft = "calc(100% - " + gripWidth + "px)";
+    grip.style.transition = "margin-left 2.5s ease-in-out";
+    after.style.transition = "width 2s ease-in-out";
+
+    if (viewport>=LAPTOP_WIDTH) {
+      grip.style.transition = "margin-left 3s ease-in-out";
+      after.style.transition = "width 3s ease-in-out";
+    }
+    else {
+      grip.style.transition = "margin-left 1s ease-in-out";
+      after.style.transition = "width 1.5s ease-in-out";
+    }
+  };
+
+  grip.ondblclick = function () {
+    before.style.width = "50%";
+    after.style.width = "50%";
+    grip.style.marginLeft = "calc(50% - " + gripWidth / 2 + "px)";
+  };
+
+  var getCoords = function (elem) {
+    var box = elem.getBoundingClientRect();
+    return box.left + pageXOffset;
+  };
+
+  var gripDownHandler = function (evtDown) {
+    var gripCoords = getCoords(grip);
+    var scaleCoords = getCoords(scale);
+    grip.style.transition = "none";
+
+    var shiftX = evtDown.pageX - gripCoords;
+
+    document.onmousemove = function (evtMove) {
+      var newLeft = evtMove.pageX - shiftX - scaleCoords;
+
+      if (newLeft < 0) {
+        newLeft = 0;
+      }
+
+      var rightEdge = scaleWidth - gripWidth;
+      if (newLeft > rightEdge) {
+        newLeft = rightEdge;
+      }
+
+      var gripValue = newLeft / rightEdge * 100;
+      grip.style.marginLeft = newLeft + "px";
+
+      before.style.width = (100 - gripValue) + "%";
+      after.style.width = gripValue + "%";
+    };
+
+    document.onmouseup = function () {
+      document.onmousemove = document.onmouseup = null;
+      grip.style.transition = "margin-left 0.2s ease-out";
+    };
+
+    return false;
+  };
+
+  var addGripHandlers = function () {
+    grip.addEventListener("mousedown", gripDownHandler);
+  };
+
+  var removeGripHandlers = function () {
+    grip.removeEventListener("mousedown", gripDownHandler);
+  };
+
+
+  var initialize = function() {
+    var viewport = document.documentElement.clientWidth || window.innerWidth;
+
+    if (viewport >= tabletWidth) {
+      addGripHandlers();
+    } else {
+      removeGripHandlers();
+    }
+
+    sliderWidth = getElemWidth(visual);
+    scaleWidth = getElemWidth(scale);
+    gripWidth = getElemWidth(grip);
+
+    // before.style.width = "";
+    // after.style.width = "";
+    // grip.style.marginLeft = "";
+  };
+
+  window.addEventListener("load", initialize);
+  window.addEventListener("resize", initialize);
+})();
